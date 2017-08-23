32575180e46b95b65dc34eaea80bfa0e59e10969
shantu <shantavir219@gmail.com>
2017-08-23 20:59:34 +0530

    Added syso statement

--- src/test/java/jenkins/Test1.java
+++ src/test/java/jenkins/Test1.java
@@ -15,6 +15,7 @@ public class Test1 {
 		  
 		  driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java/2.53.0");
 		  System.out.println("Tittle   :" + driver.getTitle());
+		  System.out.println("Hurray finally achived");
 		  
 	  }
 	  @BeforeClass
